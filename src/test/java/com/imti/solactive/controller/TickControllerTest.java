276108f51f400258b8460c0b079003f20f9dc6a3
Imteyaz Ahmad <ahmad.imt07@gmail.com>
2019-09-29 22:12:48 +0530

    Added test cases for statistics url

--- src/test/java/com/imti/solactive/controller/TickControllerTest.java
+++ src/test/java/com/imti/solactive/controller/TickControllerTest.java
@@ -1,12 +1,15 @@
 package com.imti.solactive.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.imti.solactive.model.Response;
 import com.imti.solactive.model.Tick;
 import com.imti.solactive.service.TickService;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.ArgumentMatchers;
+import org.mockito.BDDMockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -51,5 +54,38 @@ public class TickControllerTest {
             .contentType(
                 MediaType.APPLICATION_JSON_UTF8_VALUE))
         .andExpect(MockMvcResultMatchers.status().isNoContent());
+  }
+
+  @Test
+  public void shouldReturnOverallStats() throws Exception {
+
+    Response response = new Response(12.3, 34.5, 12.0, 5);
+    BDDMockito.given(tickService.getOverallStatistics()).willReturn(response);
+    mockMvc.perform(
+        MockMvcRequestBuilders.get("/statistics")
+            .contentType(
+                MediaType.APPLICATION_JSON_UTF8_VALUE))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("avg").value(12.3))
+        .andExpect(MockMvcResultMatchers.jsonPath("max").value(34.5))
+        .andExpect(MockMvcResultMatchers.jsonPath("min").value(12.0))
+        .andExpect(MockMvcResultMatchers.jsonPath("count").value(5));
+  }
+
+  @Test
+  public void shouldReturnInstrumentsStats() throws Exception {
+
+    Response response = new Response(13.8, 23.6, 12.0, 4);
+    BDDMockito.given(tickService.getStatisticsByInstrument(ArgumentMatchers.anyString()))
+        .willReturn(response);
+    mockMvc.perform(
+        MockMvcRequestBuilders.get("/statistics/IBM.N")
+            .contentType(
+                MediaType.APPLICATION_JSON_UTF8_VALUE))
+        .andExpect(MockMvcResultMatchers.status().isOk())
+        .andExpect(MockMvcResultMatchers.jsonPath("avg").value(13.8))
+        .andExpect(MockMvcResultMatchers.jsonPath("max").value(23.6))
+        .andExpect(MockMvcResultMatchers.jsonPath("min").value(12.0))
+        .andExpect(MockMvcResultMatchers.jsonPath("count").value(4));
   }
 }
